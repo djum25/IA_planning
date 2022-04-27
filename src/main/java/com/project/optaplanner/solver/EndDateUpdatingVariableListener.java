f76c812296652b69bbbc210454ba10b33da80ae5
DJUM <djum25@gmail.com>
2022-04-27 15:05:46 +0000

    change calculateDate method

--- src/main/java/com/project/optaplanner/solver/EndDateUpdatingVariableListener.java
+++ src/main/java/com/project/optaplanner/solver/EndDateUpdatingVariableListener.java
@@ -1,15 +1,23 @@
 package com.project.optaplanner.solver;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.optaplanner.core.api.domain.variable.VariableListener;
 import org.optaplanner.core.api.score.director.ScoreDirector;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.project.optaplanner.domain.Programme;
 import com.project.optaplanner.domain.Task;
+import com.project.optaplanner.service.HolidayService;
 
 
 public class EndDateUpdatingVariableListener implements VariableListener<Programme, Task>{
+
+    @Autowired
+    private HolidayService holidayService;
 
     @Override
     public void beforeEntityAdded(ScoreDirector<Programme> scoreDirector, Task task) {
@@ -42,13 +50,18 @@ public class EndDateUpdatingVariableListener implements VariableListener<Program
     }
 
     protected void updateEndDate(ScoreDirector<Programme> scoreDirector, Task task) {
-    	
         scoreDirector.beforeVariableChanged(task, "endDate");
         task.setEndDate(calculateEndDate(task.getStartDate(), task.getDuration()));
         scoreDirector.afterVariableChanged(task, "endDate");
     }
 
-    public static LocalDate calculateEndDate(LocalDate startDate, int durationInDays) {
+    public  LocalDate calculateEndDate(LocalDate startDate, int durationInDays) {
+        /* LocalDate endDate = startDate.plusDays(durationInDays);
+        boolean ref = true;
+        List<LocalDate> dates =  startDate.datesUntil(endDate).collect(Collectors.toList());
+        dates.stream().filter(date-> date.getDayOfWeek()==DayOfWeek.SATURDAY || date.getDayOfWeek()==DayOfWeek.SUNDAY
+        || holidayService.getHolidaysDate().contains(date)).count(); */
+
         if (startDate == null) {
             return null;
         } else {
