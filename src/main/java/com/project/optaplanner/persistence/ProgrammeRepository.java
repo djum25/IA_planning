e11394fe3cf66d3675580911ceff8d4fac297638
DJUM <djum25@gmail.com>
2022-04-27 15:02:18 +0000

    save holidays in programme

--- src/main/java/com/project/optaplanner/persistence/ProgrammeRepository.java
+++ src/main/java/com/project/optaplanner/persistence/ProgrammeRepository.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.project.optaplanner.domain.Programme;
 import com.project.optaplanner.domain.Task;
+import com.project.optaplanner.service.HolidayService;
 
 @Service
 @Transactional
@@ -22,12 +23,15 @@ public class ProgrammeRepository {
 	private WorkCalendarRepository workCalendarRepository;
 	@Autowired
 	private TeamRepository teamRepository;
+	@Autowired
+	private HolidayService holidayService;
 	
 	public Programme findById(Long id) {
 		if (!SINGLETON_TIME_TABLE_ID.equals(id)) {
             throw new IllegalStateException("There is no timeTable with id (" + id + ").");
         }
-		return new Programme(workCalendarRepository.findAll().get(0),prototypeRepository.findAll(),taskRepository.findAll(),teamRepository.findAll());
+		return new Programme(workCalendarRepository.findAll().get(0),prototypeRepository.findAll(),
+		taskRepository.findAll(),teamRepository.findAll(),holidayService.getHolidaysDate());
 	}
 	
 	public void save(Programme programme) {
