e437666d9885b7b85a3847f10b035a78ae3dfeba
DJUM <djum25@gmail.com>
2022-04-28 14:38:55 +0000

    updape calculate date for holidays in progress

--- src/main/java/com/project/optaplanner/solver/EndDateUpdatingVariableListener.java
+++ src/main/java/com/project/optaplanner/solver/EndDateUpdatingVariableListener.java
@@ -5,19 +5,29 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.annotation.PostConstruct;
+
 import org.optaplanner.core.api.domain.variable.VariableListener;
 import org.optaplanner.core.api.score.director.ScoreDirector;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.project.optaplanner.domain.Programme;
 import com.project.optaplanner.domain.Task;
 import com.project.optaplanner.service.HolidayService;
 
-
+@Component
 public class EndDateUpdatingVariableListener implements VariableListener<Programme, Task>{
+
+    private static HolidayService holiday;
 
     @Autowired
     private HolidayService holidayService;
+
+    @PostConstruct
+    public void init() {
+        EndDateUpdatingVariableListener.holiday = holidayService;
+    }
 
     @Override
     public void beforeEntityAdded(ScoreDirector<Programme> scoreDirector, Task task) {
@@ -55,14 +65,34 @@ public class EndDateUpdatingVariableListener implements VariableListener<Program
         scoreDirector.afterVariableChanged(task, "endDate");
     }
 
-    public  LocalDate calculateEndDate(LocalDate startDate, int durationInDays) {
-        /* LocalDate endDate = startDate.plusDays(durationInDays);
-        boolean ref = true;
-        List<LocalDate> dates =  startDate.datesUntil(endDate).collect(Collectors.toList());
-        dates.stream().filter(date-> date.getDayOfWeek()==DayOfWeek.SATURDAY || date.getDayOfWeek()==DayOfWeek.SUNDAY
-        || holidayService.getHolidaysDate().contains(date)).count(); */
-
+    public static LocalDate calculateEndDate(LocalDate startDate, int durationInDays) {
+        
         if (startDate == null) {
+            return null;
+        } else {
+            LocalDate endDate = startDate.plusDays(durationInDays);
+            boolean ref = true;
+            List<LocalDate> dates =  startDate.datesUntil(endDate).collect(Collectors.toList());
+            Long  notUse = dates.stream().filter(date-> date.getDayOfWeek()==DayOfWeek.SATURDAY || date.getDayOfWeek()==DayOfWeek.SUNDAY
+            || holiday.getHolidaysDate().contains(date)).count();
+            while(ref) {
+                if(dates.size() < durationInDays + notUse) {
+                    LocalDate newDate = dates.get(dates.size() - 1).plusDays(1);
+                    dates.add(newDate);
+                } else {
+                    notUse = dates.stream().filter(date-> date.getDayOfWeek()==DayOfWeek.SATURDAY 
+                    || date.getDayOfWeek()==DayOfWeek.SUNDAY
+                    || holiday.getHolidaysDate().contains(date)).count();
+                    if(dates.size() == (durationInDays + notUse)) {
+                        ref = false;
+                        endDate = dates.get(dates.size() - 1);
+                    }
+                }
+            }
+            return endDate;
+        }
+
+        /*if (startDate == null) {
             return null;
         } else {
             // Skip weekends. Does not work for holidays.
@@ -70,6 +100,6 @@ public class EndDateUpdatingVariableListener implements VariableListener<Program
             // Keep in sync with Programme.createStartDateList().
             int weekendPadding = 2 * ((durationInDays + (startDate.getDayOfWeek().getValue() - 1)) / 5);
             return startDate.plusDays(durationInDays + weekendPadding);
-        }
+        }*/
     }
 }
